e7c3f4e042ee74c216bbfa117830e2f810d3fedc
TomWwg <741889962@qq.com>
2018-01-08 15:24:15 +0800

    netty
    netty(1)部分

--- endoscope/src/main/java/com/iel/endoscope/netty/ServerHandler.java
+++ endoscope/src/main/java/com/iel/endoscope/netty/ServerHandler.java
@@ -1,31 +1,24 @@
 package com.iel.endoscope.netty;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
  * Created by wwg on 2017/12/29.
  * @author wwg
  */
-public class ServerHandler extends ChannelHandlerAdapter {
+public class ServerHandler extends ChannelInboundHandlerAdapter {
 
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception{
-        ByteBuf buf = (ByteBuf) msg;
-        byte[] data = new byte[buf.readableBytes()];
-        buf.readBytes(data);
-        String request = new String(data, "utf-8");
-        System.out.println("Server：" + request);
-        //写给客户端
-        String response = "我是反馈的信息";
-        ctx.writeAndFlush(Unpooled.copiedBuffer("888".getBytes()));
-        //addListner(ChannelFutureListner.CLOSE);
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress()+"===>server: "+msg.toString());
+        ctx.write("received your msg");
+        ctx.flush();
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception{
-        cause.printStackTrace();
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
         ctx.close();
     }
 }
